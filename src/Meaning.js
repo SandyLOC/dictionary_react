03f645e47933a53b3e74eabd6a772f75452fd098
SandyLOC <sandyplc@hotmail.com>
2021-06-11 22:10:19 -0700

    Conditional for example

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Synonyms from "./Synonyms.js";
 
 export default function Meaning(props) {
     console.log(props.meaning);
@@ -6,13 +7,26 @@ export default function Meaning(props) {
         <div className="Meaning">
             <h3 className="defType">{props.meaning.partOfSpeech}</h3>
             {props.meaning.definitions.map(function(definition, index) {
+                if(definition.example) {
+                    return (
+                        <div key={index}>
+                        <p>{index + 1 +".-"} <span>{definition.definition}</span> 
+                        <br/>
+                        <strong>Example: </strong>
+                        <em><span>{definition.example}</span></em>
+                        <br/>
+                        <Synonyms synonyms={definition.synonyms}/></p>
+                     </div>
+                    );
+                } else {
                 return (
                     <div key={index}>
                        <p>{index + 1 +".-"} <span>{definition.definition}</span> 
                        <br/>
-                       <em><span>{definition.example}</span></em></p>
+                       <Synonyms synonyms={definition.synonyms}/></p>
                     </div>
                 );
+                }
             })}
         </div>
     );
