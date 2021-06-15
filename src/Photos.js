6b9df4422042f54bd32e47fec2a80650ef681c50
SandyLOC <sandyplc@hotmail.com>
2021-06-14 19:17:10 -0700

    alt for image added

--- src/Photos.js
+++ src/Photos.js
@@ -10,7 +10,7 @@ export default function Photos(props) {
                         return(
                             <div className="col-3" key={index}>
                             <a href={photo.src.original} target="_blank" rel="noreferrer"> 
-                            <img src={photo.src.tiny} className="img-fluid"/></a>
+                            <img src={photo.src.tiny} className="img-fluid" alt="img"/></a>
                             </div>
                         );
                         })}
